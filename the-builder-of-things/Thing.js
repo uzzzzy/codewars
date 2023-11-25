5ac126ba4a38b4b21ba868aa6ca29eb71b91650a
uzzzzy <a.fauzy4b@gmail.com>
2023-11-25 15:42:28 +0800

    Add properties for person and man/woman

--- the-builder-of-things/Thing.js
+++ the-builder-of-things/Thing.js
@@ -5,6 +5,10 @@ class Thing {
     }
 
     this.name = name;
+
+    this.is_a_person = false;
+    this.is_a_man = false;
+    this.is_a_woman = false;
   }
 
   get is_a() {
@@ -41,42 +45,9 @@ class Thing {
     );
   }
 
-  get is_a_() {
-    const self = this;
-    return new Proxy(
-      {},
-      {
-        get(target, prop) {
-          console.log('is_a_', prop);
-          if (prop.startsWith('person')) {
-            const dynamicProp = prop.slice('person'.length);
-            return self._dynamicValues[dynamicProp];
-          } else {
-            return undefined; // or any default value/error handling as needed
-          }
-        },
-        set(target, prop, value) {
-          console.log('is_a_', prop, value);
-          if (prop.startsWith('person')) {
-            const dynamicProp = prop.slice('person'.length);
-            self._dynamicValues[dynamicProp] = value;
-            return true;
-          } else {
-            return false; // disallow setting properties not starting with 'person'
-          }
-        },
-        deleteProperty(target, prop) {
-          if (prop.startsWith('person')) {
-            const dynamicProp = prop.slice('person'.length);
-            if (dynamicProp in self._dynamicValues) {
-              delete self._dynamicValues[dynamicProp];
-              return true;
-            }
-          }
-          return false;
-        },
-      }
-    );
+  has(number) {
+    this.has_count = number;
+    return this;
   }
 }
 
--- the-builder-of-things/index.js
+++ the-builder-of-things/index.js
@@ -10,13 +10,17 @@ try {
   jane.is_a.person;
   jane.is_a.man;
   jane.is_a.woman;
+  john.is_a_man;
 
   test.describe('Jane is a person').expect(jane.is_a_person).toBe(true);
   test.describe('Jane is a man').expect(jane.is_a_man).toBe(false);
   test.describe('Jane is a woman').expect(jane.is_a_woman).toBe(true);
-
   test.describe('jane name is Jane').expect(jane.name).toBe('Jane');
+
+  test.describe('John is a person').expect(john.is_a_person).toBe(false);
   test.describe('john name is John').expect(john.name).toBe('John');
+  test.describe('John is a man').expect(john.is_a_man);
+
   test.end();
 } catch (error) {
   console.log(error.message);
