152c499c98fd4e6de512889c1295f52545fe466c
uzzzzy <a.fauzy4b@gmail.com>
2023-11-25 15:20:22 +0800

    Add test results to Test class

--- Test.js
+++ Test.js
@@ -2,6 +2,9 @@ class Test {
   constructor() {
     this.description = '';
 
+    this.passed = 0;
+    this.failed = 0;
+
     console.time('Test');
   }
 
@@ -21,12 +24,14 @@ class Test {
           error: '\x1b[31m',
         };
         if (actual !== expected) {
+          self.failed++;
           console.log(
             `-${colors.description} ${self.description} =>${colors.error} ${actual} ✗ \x1b[0m`
           );
           self.description = '';
           return;
         }
+        self.passed++;
         console.log(
           `-${colors.description} ${self.description} =>${colors.success} ${actual} ✓ \x1b[0m`
         );
@@ -36,7 +41,13 @@ class Test {
   }
 
   end() {
-    console.timeEnd('Test');
+    console.timeEnd(`Test`);
+    const colors = {
+      success: '\x1b[32m',
+      error: '\x1b[31m',
+    };
+    console.log(`${colors.success} ${this.passed} passed \x1b[0m`);
+    console.log(`${colors.error} ${this.failed} failed \x1b[0m`);
   }
 }
 
