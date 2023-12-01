6ea4e6bd3ffc7beb857af9c0ac4eb68e65c87904
uzzzzy <a.fauzy4b@gmail.com>
2023-12-01 20:20:56 +0800

    Add route calculator function and test cases

---
+++ route-calculator/index.js
@@ -0,0 +1,30 @@
+function calculate(sum) {
+  console.log('calculate', sum);
+}
+
+const testList = [
+  {
+    test: '1+1',
+    expected: 2,
+  },
+  {
+    test: '10$2',
+    expected: 5,
+  },
+  {
+    test: '1.5*3',
+    expected: 4.5,
+  },
+  {
+    test: '5+5+5+5',
+    expected: 20,
+  },
+  {
+    test: '1000$2.5$5+5-5+6$6',
+    expected: 81,
+  },
+  {
+    test: '10-9p',
+    expected: 1,
+  },
+];
