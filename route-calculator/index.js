d26c82e19531cea6788c9ba8af686b0c4d1df5c2
uzzzzy <a.fauzy4b@gmail.com>
2023-12-01 20:28:20 +0800

    Refactor route calculator function to handle operators and numbers separately

--- route-calculator/index.js
+++ route-calculator/index.js
@@ -1,5 +1,48 @@
 function calculate(sum) {
-  console.log('calculate', sum);
+  const sumArray = sum.split('');
+  const numbers = [];
+  const operators = [];
+  let currentNumber = '';
+
+  sumArray.forEach((char) => {
+    if (char === '.') {
+      currentNumber += char;
+      return;
+    }
+    if (!isNaN(char)) {
+      currentNumber += char;
+      return;
+    }
+    operators.push(char);
+    numbers.push(Number(currentNumber));
+    currentNumber = '';
+  });
+  numbers.push(Number(currentNumber));
+  console.log('numbers', numbers);
+  console.log('operators', operators);
+
+  let result = numbers[0];
+  for (let i = 0; i < operators.length; i++) {
+    const operator = operators[i];
+    const nextNumber = numbers[i + 1];
+    switch (operator) {
+      case '+':
+        result += nextNumber;
+        break;
+      case '-':
+        result -= nextNumber;
+        break;
+      case '*':
+        result *= nextNumber;
+        break;
+      case '$':
+        result /= nextNumber;
+        break;
+      default:
+        break;
+    }
+  }
+  return result;
 }
 
 const testList = [
@@ -25,7 +68,7 @@ const testList = [
   },
   {
     test: '10-9p',
-    expected: 1,
+    expected: '400: Bad Request',
   },
 ];
 
